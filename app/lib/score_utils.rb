ab14a6c8f76211496d48db67d83d5836ce97ab73
jjy <jjyruby@gmail.com>
2017-07-02 16:15:25 +0800

    fix Score_utils.hot_score

--- app/lib/score_utils.rb
+++ app/lib/score_utils.rb
@@ -4,8 +4,8 @@ module ScoreUtils
   # details http://www.evanmiller.org/deriving-the-reddit-formula.html
   # return: double
   def hot_score ups, downs, date
-    seconds = date - 1500009635 # first commit
-    (Math.log(ups + 1) - Math.log(ups + downs + 2) + seconds / 19543).round(7)
+    seconds = date - 1497446400 # first commit
+    (Math.log(ups + 1) - Math.log(ups + downs + 2) + seconds.to_f / 19543).round(7)
   end
 end
 
