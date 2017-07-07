5f0bb1ad0bbb246cf1bc6bd6af7c814b10bb442e
jjy <jjyruby@gmail.com>
2017-07-07 13:31:51 +0800

    upgrade site style

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -32,7 +32,7 @@ class TopicsController < ApplicationController
 
     respond_to do |format|
       if @topic.save
-        format.html {redirect_to @topic, notice: 'Topic was successfully created.'}
+        format.html {redirect_to @topic, notice: '成功提交话题'}
         format.json {render :show, status: :created, location: @topic}
       else
         format.html {render :new}
