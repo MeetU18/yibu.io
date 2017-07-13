bee2bd1523652902e6f997770c5ce00c99058b55
jjy <jjyruby@gmail.com>
2017-07-14 01:44:59 +0800

    Cache recommended tags

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,5 +1,7 @@
 module TagsHelper
   def recommended_tags
-    Tag.order(sequence: :asc)
+    Rails.cache.fetch("tags_helper#recommended_tags", expires_in: 12.hours) do
+      Tag.order(sequence: :asc)
+    end
   end
 end
