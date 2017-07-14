1b196d843c16fac2749fe2b8ea56eab725792a28
jjy <jjyruby@gmail.com>
2017-07-14 16:45:00 +0800

    Fix tags error, remote TODO FIX

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,8 +1,7 @@
 module TopicsHelper
-  # TODO topic.tag raise cast error, maybe a rails bug, check it later
   def render_topic_info topic, comments: false
     <<-HTML.html_safe
-      <span>#{topic.user.username} 发布于 </span>#{link_to Tag.find(topic.tag_id).name, tag_topics_path(topic.tag_id) }
+      <span>#{topic.user.username} 发布于 </span>#{link_to topic.tag.name, tag_topics_path(topic.tag.id) }
       <span> #{ time_ago_in_words topic.created_at }前 </span>
       #{link_to("查看评论(#{topic.comments_count})", topic) if comments}
     HTML
