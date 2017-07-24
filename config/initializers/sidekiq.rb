593b2773a965bb295b2fbdbb55787bcecf082227
jjy <jjyruby@gmail.com>
2017-07-24 23:41:39 +0800

    Support configure sidekiq redis size

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
 Sidekiq.configure_server do |config|
-  config.redis = {url: Settings.sidekiq.redis_url}
+  options = {url: Settings.sidekiq.redis_url}
+  options[:size] = Settings.sidekiq.server.redis_size if Settings.sidekiq.server&.redis_size
+  config.redis = options
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: Settings.sidekiq.redis_url}
+  options = {url: Settings.sidekiq.redis_url}
+  options[:size] = Settings.sidekiq.client.redis_size if Settings.sidekiq.server&.redis_size
+  config.redis = options
 end
